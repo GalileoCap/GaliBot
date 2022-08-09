64dc254da7bd8e6d673f3f3b921d6ab5a11ced8c
GalileoCap <galileocapp@gmail.com>
2022-08-09 14:54:19 -0300

    OK: Easier to check for commands and params

--- api.py
+++ api.py
@@ -2,54 +2,59 @@ import requests
 import cfg
 
 class Update:
-	def __init__(self, obj):
-		self.id = obj.get('update_id')
-		self.message = Message(obj['message']) if 'message' in obj else None
-		#TODO: Everything else as needed
-	
-	def __repr__(self):
-		return f'{{id: {self.id}, message: {self.message}}}'
+  def __init__(self, obj):
+    self.id = obj.get('update_id')
+    self.message = Message(obj['message']) if 'message' in obj else None
+    #TODO: Everything else as needed
+  
+  def __repr__(self):
+    return f'{{id: {self.id}, message: {self.message}}}'
 
 class Message:
-	def __init__(self, obj):
-		self.id = obj.get('message_id')
-		self.user = User(obj['from']) if 'from' in obj else None
-		self.chat = Chat(obj['chat']) if 'chat' in obj else None
-		self.text = obj.get('text')
-		#TODO: Everything else as needed
+  def __init__(self, obj):
+    self.id = obj.get('message_id')
+    self.user = User(obj['from']) if 'from' in obj else None
+    self.chat = Chat(obj['chat']) if 'chat' in obj else None
 
-	def __repr__(self):
-		return f'{{id: {self.id}, user: {self.user}, text: {self.text}}}'
+    self.raw = obj.get('text');
+    firstSpace, isCommand = self.raw.find(' '), len(self.raw) > 0 and self.raw[0] == '/';
+    if firstSpace == -1: firstSpace = None;
+    self.command = self.raw[1:firstSpace] if isCommand else None;
+    self.text = self.raw[firstSpace:] if isCommand else self.raw;
+    #TODO: Everything else as needed
+
+  def __repr__(self):
+    return f'{{id: {self.id}, user: {self.user}, command: {self.command}, text: {self.text}}}'
 
 class User:
-	def __init__(self, obj):
-		self.id = obj.get('id')
-		self.username = obj.get('username')
-		#TODO: Everything else as needed
+  def __init__(self, obj):
+    self.id = obj.get('id')
+    self.username = obj.get('username')
+    #TODO: Everything else as needed
 
-	def __repr__(self):
-		return f'{{id: {self.id}, username: {self.username}}}'
+  def __repr__(self):
+    return f'{{id: {self.id}, username: {self.username}}}'
 
 class Chat:
-	def __init__(self, obj):
-		self.id = obj.get('id')
-	
-	def __repr__(self):
-		return f'{{id: {self.id}}}'
+  def __init__(self, obj):
+    self.id = obj.get('id')
+  
+  def __repr__(self):
+    return f'{{id: {self.id}}}'
 
 def getUpdates(lastUpdateId = -1): #TODO: Get better params
-	r = requests.get(
-		f'https://api.telegram.org/bot{cfg.API_TOKEN}/getUpdates',
-		params = {'offset': lastUpdateId + 1}, #A: Only new updates 
-	).json()
-	if r['ok'] == 'False': print('getUpdates ERROR') #TODO: Write log and throw error
-	return [Update(obj) for obj in r['result']]
+  r = requests.get(
+    f'https://api.telegram.org/bot{cfg.API_TOKEN}/getUpdates',
+    params = {'offset': lastUpdateId + 1}, #A: Only new updates 
+  ).json()
+  if r['ok'] == 'False': print('getUpdates ERROR') #TODO: Write log and throw error
+  return [Update(obj) for obj in r['result']]
 
 def sendMessage(chatId, text, reply_to): #TODO: Get better params
-	r = requests.post(
-		f'https://api.telegram.org/bot{cfg.API_TOKEN}/sendMessage',
-		params = {'chat_id': chatId, 'text': text, 'reply_to_message_id': reply_to},
-	)
-	if r.json()['ok'] == 'False': print('sendMessage ERROR') #TODO: Write long and throw error
+  r = requests.post(
+    f'https://api.telegram.org/bot{cfg.API_TOKEN}/sendMessage',
+    params = {'chat_id': chatId, 'text': text, 'reply_to_message_id': reply_to},
+  )
+  if r.json()['ok'] == 'False': print('sendMessage ERROR') #TODO: Write long and throw error
 
 #TODO: Better handling of missing parameters in classes
--- main.py
+++ main.py
@@ -3,29 +3,35 @@ import requests
 import cfg
 
 def lastUpdateId():
-	try:
-		with open('./lastUpdate.log', 'r') as fin:
-			return int(fin.readline())
-	except FileNotFoundError:
-		#TODO: Log
-		return 0 #A: Default to 0
+  try:
+    with open('./lastUpdate.log', 'r') as fin:
+      return int(fin.readline())
+  except FileNotFoundError:
+    #TODO: Log
+    return 0 #A: Default to 0
 
 def saveLastUpdate(_id):
-	with open('./lastUpdate.log', 'w') as fin:
-		fin.write(str(_id))
+  with open('./lastUpdate.log', 'w') as fin:
+    fin.write(str(_id))
 
-def handleUpdate(update):
-	if not update.message.text is None:
-		if update.message.text == '/test':
-			api.sendMessage(update.message.chat.id, 'Test', update.message.id)
-		if update.message.text == '/ip' and update.message.user.id in cfg.chatIds:
-			api.sendMessage(update.message.chat.id, requests.get('https://api.ipify.org').text, update.message.id)
+def test(message):
+  print('handle.test');
+  api.sendMessage(message.chat.id, f'Test: {message.text}', message.id);
+
+def ip(message):
+  print('handle.ip')
+  api.sendMessage(message.chat.id, requests.get('https://api.ipify.org').text, message.id);
+
+def handleMessage(message):
+  command = message.command;
+  if command == 'test': test(message);
+  elif command == 'ip' and message.user.id in cfg.chatIds: ip(message);
 
 if __name__ == '__main__':
-	updates = api.getUpdates(lastUpdateId() + 1)
-	if len(updates) > 0:
-		print(updates)
-		for update in updates:
-			handleUpdate(update)
-		saveLastUpdate(updates[-1].id)
-	else: pass #TODO: Log no new updates
+  updates = api.getUpdates(lastUpdateId() + 1)
+  if len(updates) > 0:
+    print(updates)
+    for update in updates:
+      handleMessage(update.message)
+    saveLastUpdate(updates[-1].id)
+  else: print('No updates'); 
