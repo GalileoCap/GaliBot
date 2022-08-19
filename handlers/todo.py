724a81efcf76ef66dc3a8c7c37b7ab086b445462
GalileoCap <galileocapp@gmail.com>
2022-08-19 03:11:42 -0300

    OK: todo drop

--- handlers/todo.py
+++ handlers/todo.py
@@ -23,8 +23,22 @@ def select(message):
   df = db.getDf(dfName);
   message.respond(str(df)); #TODO: Fancier #TODO: Divide in pages
 
+def drop(message):
+  print('todo.handle.drop');
+  df = db.getDf(dfName);
+  _, ids, _ = re.split(r'^drop\s*(.*)', message.text)
+  toDrop = [int(x) for x in ids.split(',') if int(x) in df.index];
+  badIdx = [int(x) for x in ids.split(',') if not int(x) in df.index];
+  df.drop(toDrop, inplace = True);
+  db.saveDf(df, dfName);
+  message.respond('done'); #TODO: React
+  if len(badIdx) > 0:
+    message.respond(f'invalid indeces {badIdx}');
+
+
 def handle(message):
   _, subcommand, _ = re.split(r'^(\w+)', message.text); subcommand = subcommand.lower();
   if subcommand == 'add': add(message);
   elif subcommand == 'select': select(message);
+  elif subcommand == 'drop': drop(message);
   else: pass #TODO: Unknown
