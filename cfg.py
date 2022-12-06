1325131b7e64d25617cf4e0d8379aff622cd0fd7
GalileoCap <devel@galilecap.me>
2022-12-06 16:24:54 -0300

    CLEANING: Removed py

--- cfg.py
+++
@@ -1,9 +0,0 @@
-API_TOKEN = '5284340998:AAHoVpWV2Nxk-8eRlSSB67weolz8g8c5wHk' #U: API token supplied by Telegram
-chatIds = [ #U: List of chatIds to respond to
-  1129477471,
-]
-
-logDir = './log';
-lastUpdateFile = 'lastUpdate';
-
-dbDir = './db';
