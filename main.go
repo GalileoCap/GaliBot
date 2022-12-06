d0f174d5274031a808a0425cfe0e67bc3c1a0390
GalileoCap <devel@galilecap.me>
2022-12-06 14:59:49 -0300

    OK: ipUpdater

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-  tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
-  "os"
-  "encoding/json"
-  "log"
-  //"fmt"
+	"os"
+	"io/ioutil" //TODO: Use os
+	"net/http"
+	"encoding/json"
+  "fmt"
+	"log"
+	"time"
 )
 
 type Credentials struct {
@@ -27,28 +30,44 @@ func getCredentials(fpath string) Credentials {
     log.Fatalf("[getCredentials] Error decoding \"%v\": %v", fpath, err);
   }
 
-  log.Printf("[getCredentials] Successful %v", credentials);
-
   return credentials;
 }
 
-func main() {
-  credentials := getCredentials(".credentials.json"); //TODO: Configure path
-  Bot, err := tgbotapi.NewBotAPI(credentials.Token);
-  if err != nil {
-    log.Panicf("[main] Error NewBotAPI: %v", err);
+func ipUpdater(Bot *tgbotapi.BotAPI) {
+  ticker := time.NewTicker(24 * time.Hour); //TODO: Configure time
+  var ip string;
+  for range ticker.C {
+    resp, err := http.Get("https://api.ipify.org?format=text");
+    if err != nil {
+      log.Printf("[ipUpdater] Error getting IP: %v", err);
+      //TODO: Handle
+    }
+    defer resp.Body.Close();
+    
+    newIP_b, err := ioutil.ReadAll(resp.Body);
+    if err != nil {
+      log.Printf("[ipUpdater] Error reading response body: %v", err);
+      //TODO: Handle
+    }
+    newIP := string(newIP_b);
+    if ip != newIP {
+      ip = newIP;
+      log.Printf("[ipUpdater] IP changed to: %v", ip);
+      Bot.Send(tgbotapi.NewMessage(1129477471, fmt.Sprintf("[ipUpdater] newIP: %v", ip))); //TODO: Configure chatID
+    }
   }
+  //TODO: Simplify
+  //TODO: Callable
+}
 
-  //Bot.Debug = true;
-  log.Printf(Bot.Self.UserName);
-
+func listenForMessages(Bot *tgbotapi.BotAPI) {
   u := tgbotapi.NewUpdate(0); //TODO: Last update +1
   u.Timeout = 60; //TODO: What?
 
   updates := Bot.GetUpdatesChan(u);
 
   for update := range updates {
-    if update.Message != nil { // If we got a message
+    if update.Message != nil { //TODO: 
       log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
       msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
@@ -57,4 +76,17 @@ func main() {
       Bot.Send(msg)
     }
   }
+}
+
+func main() {
+  credentials := getCredentials(".credentials.json"); //TODO: Configure path
+  Bot, err := tgbotapi.NewBotAPI(credentials.Token);
+  if err != nil {
+    log.Panicf("[main] Error NewBotAPI: %v", err);
+  }
+  //Bot.Debug = true;
+  log.Printf("[main] Running as %v", Bot.Self.UserName);
+
+  go ipUpdater(Bot);
+  listenForMessages(Bot);
 }
