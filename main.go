7c311cd5d326a39c696edb6114698d55ef79ee38
GalileoCap <devel@galilecap.me>
2022-12-06 17:24:42 -0300

    OK: Allowlist with levels

--- main.go
+++ main.go
@@ -12,12 +12,23 @@ import (
 	"time"
 )
 
+var MyChatID int64 = 1129477471;
+var CurrIP string;
+
+type PermLvl int;
+const (
+  Admin PermLvl = 0;
+  Allow PermLvl = 1;
+  Block PermLvl = 2;
+)
+var Permissions = map[int64]PermLvl{
+  MyChatID: Admin,
+  5629879871: Admin,
+};
+
 type Credentials struct {
   Token string
 };
-
-var myChatID int64 = 1129477471;
-var currIP string;
 
 func getCredentials(fpath string) Credentials {
   data, err := os.ReadFile(fpath); //TODO: Config path
@@ -34,11 +45,6 @@ func getCredentials(fpath string) Credentials {
   return credentials;
 }
 
-func checkPermissions(chatID int64) bool {
-  return chatID == myChatID;
-  //TODO: Allowlist
-}
-
 func getIP() (bool, error) {
   resp, err := http.Get("https://api.ipify.org?format=text");
   if err != nil {
@@ -53,15 +59,14 @@ func getIP() (bool, error) {
     return false, err;
   }
   newIP := string(newIP_b);
-  prevIP := currIP;
-  currIP = newIP;
+  prevIP := CurrIP;
+  CurrIP = newIP;
 
   return prevIP != newIP, nil;
 } 
 
 func ipUpdater(Bot *tgbotapi.BotAPI) {
-  //ticker := time.NewTicker(24 * time.Hour); //TODO: Configure time
-  ticker := time.NewTicker(time.Second); //TODO: Configure time
+  ticker := time.NewTicker(24 * time.Hour); //TODO: Configure time
   for range ticker.C {
     changed, err := getIP();
     if err != nil {
@@ -71,7 +76,7 @@ func ipUpdater(Bot *tgbotapi.BotAPI) {
     }
 
     if changed {
-      Bot.Send(tgbotapi.NewMessage(myChatID, fmt.Sprintf("[ipUpdater] newIP: %v", currIP)));
+      Bot.Send(tgbotapi.NewMessage(MyChatID, fmt.Sprintf("[ipUpdater] newIP: %v", CurrIP)));
     }
   }
 }
@@ -82,6 +87,7 @@ func listenForMessages(Bot *tgbotapi.BotAPI) {
 
   updates := Bot.GetUpdatesChan(u);
 
+  OUTER:
   for update := range updates {
     chatID := update.FromChat().ID; message := update.Message; //A: Rename
     log.Printf("[listenForMessages] Received update from: %v", chatID);
@@ -96,20 +102,22 @@ func listenForMessages(Bot *tgbotapi.BotAPI) {
     msg := tgbotapi.NewMessage(chatID, "");
     msg.ReplyToMessageID = message.MessageID;
 
-    if checkPermissions(chatID) {
+    if permLvl, ok := Permissions[chatID]; ok && permLvl != Block {
       switch update.Message.Command() {
         case "ping": msg.Text = "pong";
         case "ip":
-          //TODO: Higher permissions
+          if permLvl != Admin {
+            continue OUTER; //A: Silent
+          }
           if _, err := getIP(); err != nil {
             msg.Text = "Error getting IP";
           } else {
-            msg.Text = fmt.Sprintf("IP: %v", currIP);
+            msg.Text = fmt.Sprintf("IP: %v", CurrIP);
           }
         default: msg.Text = fmt.Sprintf("Unknown command: /%v", message.Command());
       }
     } else {
-      msg.Text = "You're not in the allowlist";
+      msg.Text = "You're not in the allowlist, please ask your local admin";
     }
 
     if _, err := Bot.Send(msg); err != nil {
