6c9236305292422658740ebf90b916815f2a3f12
GalileoCap <devel@galilecap.me>
2022-12-21 20:34:10 -0300

    OK: Get token from arg/config file

--- src/main.go
+++ src/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-  //tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+);
 
-  "log"
-)
+type ConfigT struct {
+  Token string
+  TestToken string `json=",omitempty"`
+  Test bool
+};
+var Config ConfigT;
+
+func parseConfig(path string, test bool) {
+  data, err := os.ReadFile(path);
+  if err != nil {
+    log.Fatalf("[readToken] %v", err);
+  }
+
+  if err = json.Unmarshal(data, &Config); err != nil {
+    log.Fatalf("[readToken] %v", err);
+  }
+
+  Config.Test = test;
+}
 
 func main() {
-  log.Print("Ahoy!");
+  //A: Register command flags
+  configPath := flag.String("configPath", "config.json", "Path to the config file");
+  apiToken := flag.String("token", "", "Your bot's API token (precedence over config and test)");
+  test := flag.Bool("test", false, "Run in test mode (requires TestToken in config or --apiToken, precedence over config)");
+  flag.Parse();
+
+  parseConfig(*configPath, *test);
+
+  if *apiToken == "" { //A: Make sure to have the API token
+    if Config.Test {
+      *apiToken = Config.TestToken;
+    } else {
+      *apiToken = Config.Token;
+    }
+  }
 }
